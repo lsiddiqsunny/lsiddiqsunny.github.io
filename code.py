793f8830a581bdba977b6874b303246506478550
Latif Siddiq Sunny <lsiddiqsunny@gmail.com>
2022-09-20 01:57:46 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,10 @@
-def showUsers(username):
+import hashlib
+def hashCompare(text):
     """
-    Get user information from the MySQL database with the given username
+    Calculate the hash of the text using a common hash function and compare it
     """
-    # Connect to the database
-    db = MySQLdb.connect(host="localhost", user="root", passwd="password", db="test")
-    # Create a cursor object
-    cursor = db.cursor()
-    # Execute the SQL command
-    cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
-    # Fetch all the rows in a list of lists.
-    results = cursor.fetchall()
-    # Close the cursor
-    cursor.close()
-    # Close the database connection
-    db.close()
-    # Return the results
-    return results
+    hash = hashlib.md5(text.encode()).hexdigest()
+    if hash == "5f4dcc3b5aa765d61d8327deb882cf99":
+        print("Hash matches!")
+    else:
+        print("Hash does not match!")
